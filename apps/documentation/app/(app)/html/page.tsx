8e21b1bc6142e212c2cbb4c6c6e3560ad20bbc8e
Mike Majara <fmiguel05@gmail.com>
2025-11-17 15:23:27 +0100

    feat(documentation): integrate Prettier for HTML formatting and enhance tabbed interface
    - Added Prettier as a dependency for HTML formatting in the documentation app.
    - Implemented code highlighting using Shiki for improved visual presentation of HTML code.
    - Updated the tabbed interface to display formatted HTML output and ensure better user experience.
    - Enhanced global styles for improved layout and scrolling behavior in the documentation components.

--- apps/documentation/app/(app)/html/page.tsx
+++ apps/documentation/app/(app)/html/page.tsx
@@ -4,6 +4,9 @@ import { RecordNotionCMS, NotionCMS } from "@/lib/notion"
 import { blocksToHtml } from "@notion-utils/html"
 import React from "react"
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs"
+import { codeToHtml } from "shiki"
+import prettier from "prettier/standalone"
+import parserHtml from "prettier/plugins/html"
 
 export default async function page() {
   const notionCMS = new NotionCMS(process.env.NOTION_API_KEY!)
@@ -16,40 +19,43 @@ export default async function page() {
   }
   const content = await notionCMS.getPageContent(page.id as string)
   const html = await blocksToHtml(content)
+  const formattedHtml = await prettier.format(html, {
+    parser: "html",
+    plugins: [parserHtml]
+  })
+  const shikiHtml = await codeToHtml(formattedHtml, {
+    lang: "html",
+    theme: "github-light"
+  })
+
   return (
-    <div className="container flex gap-4 mx-auto">
-      <div className="w-1/2 border">
+    <div className="container flex gap-4 mx-auto min-h-screen">
+      <div className="overflow-hidden w-1/2 border">
         <h1>Notion Page</h1>
         <iframe
           className="w-full h-full"
           src="https://mikemajara.notion.site/ebd/29e2a789c1fc8009b74ff8e7aba151ca"
         />
       </div>
-      <div className="p-2 w-1/2 border">
+      <div className="overflow-hidden overflow-y-scroll p-2 w-1/2 h-screen border">
         <Tabs defaultValue="html">
           <TabsList>
             <TabsTrigger value="html">Plain HTML</TabsTrigger>
             <TabsTrigger value="rendered">Rendered HTML</TabsTrigger>
           </TabsList>
           <TabsContent value="html">
-            <div className="">
-              <h1>Plain HTML text</h1>
-              <div>
-                <pre
-                  style={{ wordWrap: "break-word", whiteSpace: "pre-wrap" }}
-                  className="font-light"
-                >
-                  <code className="language-html">{html}</code>
-                </pre>
-              </div>
-            </div>
+            <div
+              className="overflow-x-scroll w-full h-screen [&_.shiki]:bg-transparent!"
+              style={{ wordWrap: "break-word", whiteSpace: "pre-wrap" }}
+              dangerouslySetInnerHTML={{ __html: shikiHtml }}
+            />
           </TabsContent>
           <TabsContent value="rendered">
-            <div className="">
-              <h1>Rendered HTML text</h1>
-              <div className="prose">
-                <div dangerouslySetInnerHTML={{ __html: html }} />
-              </div>
+            <div className="overflow-y-scroll h-full">
+              <div
+                className="prose"
+                dangerouslySetInnerHTML={{ __html: html }}
+              />
             </div>
           </TabsContent>
         </Tabs>
